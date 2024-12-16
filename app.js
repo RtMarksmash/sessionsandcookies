e4013873b87d6395f60f792c3c7423386b089a6a
mario mora <jorgemariomoramolina@gmail.com>
2024-12-15 21:13:35 -0500

    setting to  auth login

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
+const dotenv = require('dotenv').config();
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+
+const user_db = process.env.DB_USER;
+const password_db = process.env.DB_PASSWORD;
+console.log(user_db)
+
+const url = `mongodb+srv://${user_db}:${password_db}@cluster0.z9aglvc.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0`
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,12 +28,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findById('67380f7f5614843fd652d913')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -35,24 +43,26 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
+    .connect(
+        url
+    )
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'mario',
+                    email: 'mario@mora.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
         });
-        user.save();
-      }
+        app.listen(config.port, () => {
+            console.log(config.message)
+        });
+    })
+    .catch(err => {
+        console.log(err);
     });
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    port: process.env.PORT || 3000,
+    message: process.env.MESSAGE || 'localhost:3000'
+}
+
+
+module.exports = config;
